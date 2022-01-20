adfaa956b3328b2d9e2f05f14ebcfe00220b4a56
Roman <getmepower@protonmail.com>
2022-01-20 05:30:05 +0400

    Initial commit

---
+++ src/test/java/com/netcracker/hwapp/HwAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcracker.hwapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
