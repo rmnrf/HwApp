9561b8df6f4958ad6c9744a98a025f3a348716f8
Roman <getmepower@protonmail.com>
2022-02-01 23:51:36 +0400

    Update Solution.java
    OnDelete annotation for cascade action

--- src/main/java/com/netcracker/hwapp/model/Solution.java
+++ src/main/java/com/netcracker/hwapp/model/Solution.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
@@ -29,7 +31,8 @@ public class Solution {
     @UpdateTimestamp
     private LocalDateTime updateDateTime;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.REFRESH)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Task task;
 
     @OneToOne
